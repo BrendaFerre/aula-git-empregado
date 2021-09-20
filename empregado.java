fdef29e52a9cbae99f20f8703bd8f61f96d96e0f
Brenda <brendah.alf@gmail.com>
2021-09-20 19:46:43 -0300

    commit inicial. Classe empregado criada

---
+++ empregado.java
@@ -0,0 +1,24 @@
+public class Empregado{
+        private String nome;
+        private int idade;
+
+        public void setNome(String nome)
+        {
+                this.nome = nome;
+        }
+        
+        public String getNome()
+        {
+                return
+        }
+
+        public void setIdade(int idade)
+        {
+                this.idade = idade;
+        }
+        
+        public int getIdade()
+        {
+                return
+        }
+}
