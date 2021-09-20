44992ae45c6dd04369c58b97ea43054ce13d55bd
Brenda <brendah.alf@gmail.com>
2021-09-20 20:07:01 -0300

    Cálculo de salário efetuado

--- empregado.java
+++ empregado.java
@@ -1,6 +1,26 @@
 public class Empregado{
         private String nome;
         private int idade;
+        private int tipo;
+        private double salario;
+        private double comissao;
+        private double bonus;
+
+        public double calculoSalario()
+        {
+                if(tipo == 1){
+                        return salario;
+                }
+                else if(tipo == 2){
+                             return salario + salario * comissao;
+                }
+                else if(tipo == 3){
+                             return salario + bonus;
+                }
+                else {
+                       return 0;
+                }
+        }
 
         public void setNome(String nome)
         {
